24b192f51b031af028cfb79c49296c13c8a9ee12
Alican Uver <can.uverr@gmail.com>
2020-08-01 13:56:35 +0300

    plugins and scss packages added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
     output: {
-        path: path.resolve(__dirname,'bundles'),
+        path: path.resolve(__dirname, 'bundles'),
         filename: 'bundle.js'
     },
     mode: 'production',
     module: {
-        rules : [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env']
                     }
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                use: 'style-loader'
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
         ]
-    }
+    },
+    devServer: {
+        port: 3200,
+        contentBase: path.join(__dirname, './bundles'),
+        watchContentBase: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
 }
